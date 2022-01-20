85a8aeee483354e4ed3989a8378eebbbf58f1aa6
VladLugovskiy <74325987+VladLugovskiy@users.noreply.github.com>
2022-01-20 10:50:29 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,54 @@
+import time
+import requests
+import telebot
+from bs4 import BeautifulSoup
+from pyowm import OWM
+from pyowm.utils.config import get_default_config
+
+bot = telebot.TeleBot("5198083670:AAH8Td_aoWI9a1BIQcAxMDFmtC_BuzqwJoU")
+
+@bot.message_handler(commands=['start'])
+def welcome(message):
+	bot.send_message(message.chat.id, 'Добро пожаловать, ' + str(message.from_user.first_name) + ',\nЧтобы узнать погоду напишите в чат название города')
+
+@bot.message_handler(content_types=['text'])
+def test(message):
+	try:
+		place = message.text
+
+		config_dict = get_default_config()
+		config_dict['language'] = 'ru'
+
+		owm = OWM('bb78a35feaa4f01ddd88f0b0e68f6574', config_dict)
+		mgr = owm.weather_manager()
+		observation = mgr.weather_at_place(place)
+		w = observation.weather
+
+		t = w.temperature("celsius")
+		t1 = t['temp']
+		t2 = t['feels_like']
+		t3 = t['temp_max']
+		t4 = t['temp_min']
+
+		wi = w.wind()['speed']
+		humi = w.humidity
+		cl = w.clouds
+		st = w.status
+		dt = w.detailed_status
+		ti = w.reference_time('iso')
+		pr = w.pressure['press']
+		vd = w.visibility_distance
+
+		bot.send_message(message.chat.id, "В городе " + str(place) + " температура " + str(t1) + " °C" + "\n" +
+				"Максимальная температура " + str(t3) + " °C" +"\n" +
+				"Минимальная температура " + str(t4) + " °C" + "\n" +
+				"Ощущается как" + str(t2) + " °C" + "\n" +
+				"Скорость ветра " + str(wi) + " м/с" + "\n" +
+				"Давление " + str(pr) + " мм.рт.ст" + "\n" +
+				"Влажность " + str(humi) + " %" + "\n")
+
+	except:
+		bot.send_message(message.chat.id,"Такой город не найден!")
+		print(str(message.text),"- не найден")
+
+bot.polling(none_stop=True, interval=0)
