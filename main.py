f47e016d97308c52fb0100dd69659624296568d4
VladLugovskiy <74325987+VladLugovskiy@users.noreply.github.com>
2022-01-20 16:37:00 +0300

    New ability
    Added the ability to find out the latest news through the bot

--- main.py
+++ main.py
@@ -1,36 +1,58 @@
+import requests
 import telebot
+from bs4 import BeautifulSoup
 from pyowm import OWM
 from pyowm.utils.config import get_default_config
 
-bot = telebot.TeleBot("Your_bot_Token")
+bot = telebot.TeleBot("5198083670:AAH8Td_aoWI9a1BIQcAxMDFmtC_BuzqwJoU")
 
 @bot.message_handler(commands=['start'])
-def welcome(message):
-	bot.send_message(message.chat.id, 'Добро пожаловать, ' + str(message.from_user.first_name) + ',\nЧтобы узнать погоду напишите в чат название города')
+def start(message):
+	bot.send_message(message.chat.id, 'Добро пожаловать, ' + str(message.from_user.first_name) +
+					 ',\nЧтобы узнать погоду напишите в чат название города' +
+					 ',\nЧтобы узнать последние новости напишите в чат "/Новости"' +
+					 ',\nЕсли забыли команды напишите в чат "/Помощь"')
+
+@bot.message_handler(commands=['Новости'])
+def start(message):
+	text = message.text
+	if text == "новости" or "Новости":
+		page = requests.get("https://ria.ru/world/")
+		x = page.content
+		soup = BeautifulSoup(x, 'html.parser')
+		html = soup.findAll('a', class_="list-item__title color-font-hover-only")
+		news = []
+		for data in html:
+			news.append(data.get('href'))
+			if len(news) == 3:
+				break
+		for i in news:
+			bot.send_message(message.chat.id, text=i)
+
+@bot.message_handler(commands=['Помощь'])
+def start(message):
+	bot.send_message(message.chat.id, 'Добро пожаловать, ' + str(message.from_user.first_name) +
+					 ',\nЧтобы узнать погоду напишите в чат название города' +
+					 ',\nЧтобы узнать последние новости напишите в чат "/Новости"')
 
 @bot.message_handler(content_types=['text'])
 def test(message):
 	try:
 		place = message.text
-
 		config_dict = get_default_config()
 		config_dict['language'] = 'ru'
-
-		owm = OWM('Your_API_key', config_dict)
+		owm = OWM('bb78a35feaa4f01ddd88f0b0e68f6574', config_dict)
 		mgr = owm.weather_manager()
 		observation = mgr.weather_at_place(place)
 		w = observation.weather
-
 		t = w.temperature("celsius")
 		t1 = t['temp']
 		t2 = t['feels_like']
 		t3 = t['temp_max']
 		t4 = t['temp_min']
-
 		wi = w.wind()['speed']
 		humi = w.humidity
 		pr = w.pressure['press']
-
 		bot.send_message(message.chat.id, "В городе " + str(place) + " температура " + str(t1) + " °C" + "\n" +
 				"Максимальная температура " + str(t3) + " °C" +"\n" +
 				"Минимальная температура " + str(t4) + " °C" + "\n" +
@@ -38,9 +60,8 @@ def test(message):
 				"Скорость ветра " + str(wi) + " м/с" + "\n" +
 				"Давление " + str(pr) + " мм.рт.ст" + "\n" +
 				"Влажность " + str(humi) + " %" + "\n")
-
 	except:
-		bot.send_message(message.chat.id,"Такой город не найден!")
-		print(str(message.text),"- не найден")
+		bot.send_message(message.chat.id, "Такой город не найден!")
+		print(str(message.text), "- не найден")
 
 bot.polling(none_stop=True, interval=0)
